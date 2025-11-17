559141e710040be1045d707190b752e64c5227b5
Nick S. <job-offers@nicks.guru>
2025-11-17 15:05:57 +0300

    Overhauled retrier and circuit breaker.

--- src/main/java/guru/nicks/notification/impl/EmailServiceImpl.java
+++ src/main/java/guru/nicks/notification/impl/EmailServiceImpl.java
@@ -2,9 +2,11 @@ package guru.nicks.notification.impl;
 
 import guru.nicks.notification.service.EmailService;
 import guru.nicks.service.FreemarkerTemplateService;
+import guru.nicks.utils.Resilience4jUtils;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.decorators.Decorators;
+import io.github.resilience4j.retry.Retry;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
@@ -15,23 +17,29 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import java.util.Map;
 
 /**
- * Renders template with {@link FreemarkerTemplateService} and sends message via {@link JavaMailSender} (configured
- * elsewhere). Leverages retries and circuit breaking using
- * <a href="https://resilience4j.readme.io/docs/getting-started-3#configuration">Resilience4j configuration</a>
- * {@value #RESILIENCE4J_CONFIG_NAME}, if any (otherwise, default values apply).
+ * Renders templates with {@link FreemarkerTemplateService} and sends messages via {@link JavaMailSender}. Leverages
+ * retries and circuit breaking (see
+ * <a href="https://resilience4j.readme.io/docs/getting-started-3#configuration">Resilience4j configuration</a>)
+ * by calling {@link #createRetrier()} and {@link #createCircuitBreaker()} from the constructor.
  */
 @RequiredArgsConstructor
 @Slf4j
 public class EmailServiceImpl implements EmailService {
 
-    public static final String RESILIENCE4J_CONFIG_NAME = "sendEmail";
-
-    // DI
     private final JavaMailSender mailSender;
     private final FreemarkerTemplateService templateService;
 
-    @Retry(name = RESILIENCE4J_CONFIG_NAME)
-    @CircuitBreaker(name = RESILIENCE4J_CONFIG_NAME)
+    private final Retry retrier;
+    private final CircuitBreaker circuitBreaker;
+
+    public EmailServiceImpl(JavaMailSender mailSender, FreemarkerTemplateService templateService) {
+        this.mailSender = mailSender;
+        this.templateService = templateService;
+
+        retrier = createRetrier();
+        circuitBreaker = createCircuitBreaker();
+    }
+
     @Override
     public void sendHtmlWithTemplate(String from, String to, String subject,
             String templateName, Map<?, ?> templateContext) {
@@ -39,8 +47,6 @@ public class EmailServiceImpl implements EmailService {
         sendHtml(from, to, subject, body);
     }
 
-    @Retry(name = RESILIENCE4J_CONFIG_NAME)
-    @CircuitBreaker(name = RESILIENCE4J_CONFIG_NAME)
     @Override
     public void sendHtml(String from, String to, String subject, String body) {
         MimeMessage message = mailSender.createMimeMessage();
@@ -64,7 +70,36 @@ public class EmailServiceImpl implements EmailService {
                     + "to='{}', subject='{}'", to, subject);
         }
 
-        mailSender.send(message);
+        Decorators.ofRunnable(() -> mailSender.send(message))
+                .withRetry(retrier)
+                .withCircuitBreaker(circuitBreaker)
+                .run();
+    }
+
+    /**
+     * Creates a {@link Retry} for handling email sending failures. Default implementation calls
+     * {@link Resilience4jUtils#createDefaultRetrier(String)} to create a retrier with a default configuration,
+     * identified by the name of this class (i.e. possibly a subclass).
+     * <p>
+     * WARNING: this method is called by the constructor, which means the object state is undefined.
+     *
+     * @return {@link Retry} instance.
+     */
+    protected Retry createRetrier() {
+        return Resilience4jUtils.createDefaultRetrier(getClass().getName());
+    }
+
+    /**
+     * Creates a {@link CircuitBreaker} for handling email sending failures. Default implementation calls
+     * {@link Resilience4jUtils#createDefaultCircuitBreaker(String)} to create a circuit breaker with a default
+     * configuration, identified by the name of this class (i.e. possibly a subclass).
+     * <p>
+     * WARNING: this method is called by the constructor, which means the object state is undefined.
+     *
+     * @return {@link CircuitBreaker} instance.
+     */
+    protected CircuitBreaker createCircuitBreaker() {
+        return Resilience4jUtils.createDefaultCircuitBreaker(getClass().getName());
     }
 
 }
