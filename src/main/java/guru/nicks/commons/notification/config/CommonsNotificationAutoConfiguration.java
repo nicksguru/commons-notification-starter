feaa3572ef3e36cf08826db11badcc4867ea865b
Nick S. <job-offers@nicks.guru>
2025-11-19 15:22:35 +0300

    Bumped version number

--- src/main/java/guru/nicks/commons/notification/config/CommonsNotificationAutoConfiguration.java
+++ src/main/java/guru/nicks/commons/notification/config/CommonsNotificationAutoConfiguration.java
@@ -4,17 +4,23 @@ import guru.nicks.commons.notification.impl.EmailServiceImpl;
 import guru.nicks.commons.notification.service.EmailService;
 import guru.nicks.commons.service.FreemarkerTemplateService;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 
 @Configuration(proxyBeanMethods = false)
+@Slf4j
 public class CommonsNotificationAutoConfiguration {
 
+    /**
+     * Creates {@link EmailService} bean if it's not already present.
+     */
     @ConditionalOnMissingBean(EmailService.class)
     @Bean
     public EmailService emailService(JavaMailSender mailSender, FreemarkerTemplateService templateService) {
+        log.debug("Building {} bean", EmailService.class.getSimpleName());
         return new EmailServiceImpl(mailSender, templateService);
     }
 
